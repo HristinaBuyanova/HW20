215c0671275a8e00420532d2b4a177feb2777184
HristinaBuyanova <hristina-a@mail.ru>
2023-12-13 23:00:47 +0300

    Add func decode and get data

--- HW20.playground/Contents.swift
+++ HW20.playground/Contents.swift
@@ -1,6 +1,29 @@
 import Foundation
 
-private func createURL(host: String, path: String, queryItem: [URLQueryItem]? = nil) -> URL? {
+struct MagicCards: Codable {
+    let cards: [ParametrCard]
+}
+
+struct ParametrCard: Codable {
+    let name: String
+    let type: String
+    let cmc: Int
+    let rarity: String
+    let cardSet: String
+    let artist: String
+    let id: String
+
+    enum CodingKeys: String, CodingKey {
+            case name, type, cmc, rarity
+            case cardSet = "set"
+            case artist, id
+        }
+}
+
+
+func createURL(host: String,
+                       path: String,
+                       queryItem: [URLQueryItem]? = nil) -> URL? {
     var components = URLComponents()
     components.scheme = "https"
     components.host = host
@@ -9,6 +32,65 @@ private func createURL(host: String, path: String, queryItem: [URLQueryItem]? = 
     return components.url
 }
 
-func getData () {
-    
+
+func decodeData(_ data: Data) throws -> MagicCards {
+    let decoder = JSONDecoder()
+    let result = try decoder.decode(MagicCards.self, from: data)
+    return result
 }
+
+func getData(urlRequest: String) {
+    let urlRequest = URL(string: urlRequest)
+    guard let url = urlRequest else {
+        print("Неверный формат ссылки")
+        return }
+    URLSession.shared.dataTask(with: url) { data, responce, error in
+        if error != nil {
+            print("Error: \(String(describing: error))")
+        } else if let responce = responce as? HTTPURLResponse {
+            switch responce.statusCode {
+            case 200:
+                print("Успешно.")
+            default:
+                print("Код ответа: \(responce.statusCode)")
+            }
+
+            guard let data = data else { return }
+
+
+            do {
+                let result = try decodeData(data)
+                if !result.cards.isEmpty {
+                    for item in result.cards {
+                        print("""
+    Название карты: \(item.name)
+    Тип карты: \(item.type)
+    Преобразованная стоимость маны: \(item.cmc)
+    Редкость карты: \(item.rarity)
+    Набор: \(item.cardSet)
+    Автор карты: \(item.artist)
+    ID: \(item.artist)
+    ----------------------------------------------
+    """)
+                    }
+
+                }
+            } catch {
+                print("Ошибка декодирования.")
+            }
+
+        }
+    }.resume()
+}
+
+
+let path = "/v1/cards"
+let host = "api.magicthegathering.io"
+let queryItemOpt = [URLQueryItem(name: "name", value: "Opt")]
+let queryItemBlackLotus = [URLQueryItem(name: "name", value: "Black Lotus")]
+
+let urlMy = createURL(host: host, path: path, queryItem: queryItemOpt)?.absoluteString
+
+getData(urlRequest: urlMy ?? "https://api.magicthegathering.io/v1/cards")
+
+
